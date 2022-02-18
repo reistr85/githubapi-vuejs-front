8388ea42e58386af72086461e9fb16912fcb237a
Renan Reis <renan.reis@locaweb.com.br>
2022-02-18 18:40:15 -0300

    feat(favorites): created component

--- src/views/Favorites/pages/Favorites.vue
+++ src/views/Favorites/pages/Favorites.vue
@@ -1,13 +1,66 @@
 <template>
-  <div>Favorites</div>
+  <v-card elevation="2" max-width="1000" class="mx-auto my-12 cardRepositories">
+    <v-card-title>
+      Repositórios Favoritos
+    </v-card-title>
+    <v-divider></v-divider>
+    
+    <v-card-content>
+      <v-list two-line>
+        <v-list-item v-for="repository in repositories" :key="repository.id">
+        <v-list-item-content class="d-flex list-item-content">
+          <div class="mr-5">
+            <v-img width="80" :src="repository.avatar_url"></v-img>
+          </div>
+
+          <div class="content-main">
+            <v-list-item-title v-html="repository.name"></v-list-item-title>
+            <v-list-item-subtitle v-html="repository.description"></v-list-item-subtitle>
+            <v-list-item-subtitle v-html="repository.url" class="mt-3"></v-list-item-subtitle>
+            <v-chip class="mt-3" size="x-small" color="primary">{{ repository.stargazers_count }} <v-icon>mdi-star</v-icon></v-chip>
+            
+            <br>
+            <v-btn color="secondary" rounded="lg" size="small" class="mt-5" :loading="loading" @click="destroyRepository(repository.id)">
+              <v-icon>mdi-star</v-icon>
+              Desfavoritar
+            </v-btn>
+            <v-divider class="mt-3"></v-divider>
+          </div>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card-content>
+  </v-card>
 </template>
 
 <script>
-export default {
-  name: 'Favorites'
-}
+import axios from '../../../service/index.js';
+  export default {
+    data: () => ({
+      repositories: [],
+    }),
+    mounted() {
+      this.getRepositories();
+    },
+    methods: {
+      async getRepositories() {
+        const res = await axios.get('/repositories');
+        this.repositories = res.data;
+      },
+      async destroyRepository(id) {
+        await axios.delete(`/repositories/${id}`);
+        this.getRepositories();
+      },
+    }
+  }
 </script>
 
-<style>
+<style scoped>
+.v-list {
+  overflow: hidden !important;
+}
 
+.list-item-content, .content-main {
+  width: 100% !important;
+}
 </style>
--- src/views/GitHub/pages/SearchRepository.vue
+++ src/views/GitHub/pages/SearchRepository.vue
@@ -1,37 +1,50 @@
 <template>
+  <v-alert type="success" v-if="success">Repositório favoritado com sucesso.</v-alert>
+  <v-alert type="error" v-if="error">{{ errorMessage }}</v-alert>
+  
   <v-card elevation="2" max-width="1000" class="mx-auto my-12">
     <v-card-title>
       Pesquisar Repositório no GitHub
     </v-card-title>
     <v-divider></v-divider>
 
-    <v-card-text>
-      <v-text-field v-model="search" label="Nome do repositório Git" placeholder="digite o nome" outlined dense></v-text-field>
-    </v-card-text>
+    <v-card-content>
+      <v-text-field v-model="search" label="Digite nome do repositório Git" placeholder="digite o nome"  outlined dense></v-text-field>
+    </v-card-content>
 
     <v-divider></v-divider>
     <v-card-actions>
-      <v-btn color="primary" @click="actionSearch()">Pesquisar</v-btn>
+      <v-btn color="primary" rounded="lg" :loading="loading" @click="actionSearch()">
+        <v-icon>mdi-check-circle</v-icon>
+        Pesquisar
+      </v-btn>
     </v-card-actions>
   </v-card>
 
-  <v-card max-width="1000" class="mx-auto">
+  <v-card max-width="1000" class="mx-auto" v-if="displayCard">
     <v-list three-line>
       <v-card-title>{{ repository.full_name }}</v-card-title>
       <v-divider></v-divider>
 
-        <v-card-text>
+        <v-card-content>
           <v-list-item>
-            <v-list-item-avatar>
-              <v-img :src="repository.organization?.avatar_url"></v-img>
-            </v-list-item-avatar>
+            <v-img max-width="80" :src="repository.organization?.avatar_url" class="mr-5"></v-img>
 
             <v-list-item-content>
               <v-list-item-title>{{ repository.description }}</v-list-item-title>
-              <v-list-item-subtitle>{{ repository.avatar_url }}</v-list-item-subtitle>
+              <v-list-item-subtitle>{{ repository.html_url }}</v-list-item-subtitle>
+              <v-chip class="mt-3" size="x-small"  color="primary">{{ repository.stargazers_count }} <v-icon>mdi-star</v-icon></v-chip>
             </v-list-item-content>
           </v-list-item>
-        </v-card-text>
+        </v-card-content>
+
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="secondary" rounded="lg" :loading="loading" @click="actionFavorite()">
+            <v-icon>mdi-star</v-icon>
+            Favoritar
+          </v-btn>
+        </v-card-actions>
     </v-list>
   </v-card>
 </template>
@@ -43,16 +56,46 @@ export default {
   name: 'SearchRepository',
   data() {
     return {
-      search: '',
-      repository: {}
+      search: 'vuejs/vue',
+      repository: {},
+      loader: false,
+      displayCard: false,
+      success: false,
+      error: false,
+      errorMessage: '',
     }
   },
   methods: {
     async actionSearch() {
       const res = await axios.post('/github/get-repository-by-name', { name: this.search });
+      this.repository = res.data[0];
+      this.displayCard = true;
+      this.success = false;
+      this.error = false;
+    },
+    async actionFavorite() {
+      const params = {
+        github_id: this.repository.id,
+        name: this.repository.full_name,
+        description: this.repository.description,
+        url: this.repository.html_url,
+        avatar_url: this.repository.organization.avatar_url,
+        stargazers_count: this.repository.stargazers_count,
+        language: this.repository.language
+      }
 
-     this.repository = res.data[0];
-    }
+      await axios.post('/repositories', params).then(() => {
+        this.repository = {}
+        this.displayCard = false;
+        this.success = true;
+      }).catch((err) => {
+        this.errorMessage = 'Erro ao favoritar';
+        if(err.response?.data?.error)
+          this.errorMessage = err.response.data.message
+        this.error = true;
+      });
+      
+    },
   }
 }
 </script>
